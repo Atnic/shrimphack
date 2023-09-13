7f450ae888b52f598c8d2a1532aa29c53e1521a2
Syauqy Nurul Aziz <syauqyaziz@gmail.com>
2023-09-13 12:45:17 +0700

    fix(home): increasing page audit score

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -4,13 +4,14 @@ import { InstagramLogo } from "../logo/social-logo";
 export function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="flex flex-row justify-between p-8 lg:px-16 items-center">
+    <div className="flex flex-row justify-between p-8 px-4 lg:px-16 items-center">
       <div className="font-light">Copyright © {currentYear} ShimpHack</div>
-      <div>
+      <div className="flex items-center hover:-translate-y-1 delay-75">
         <a
           href="https://instagram.com/lifeatjala"
           target="_blank"
           rel="noreferrer"
+          className="inline-flex items-center"
         >
           <InstagramLogo
             width={30}
@@ -18,6 +19,7 @@ export function Footer() {
             fill={"white"}
             aria-label={"Instagram Life at Jala"}
           />
+          lifeatjala
         </a>
       </div>
     </div>
